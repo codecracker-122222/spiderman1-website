399764b086cabf94336b7b8893a5681cdac18f51
KARTHIKEYAN <velkumara979@gmail.com>
2026-01-03 20:09:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const nextButton = document.querySelector('.next-button');
+const video = document.querySelector('hero-video');
+
+const movieList = [videos/hero1.mp4,videos/hero2.mp4,videos/hero3.mp4,videos/hero4.mp4];
+let index = 0;
+nextButton.addEventListener('click',function(){
+    index += 1
+    videos.src = movieList[index];
+    if (index === 3){
+       index = -1;
+    }
+})
